afc3db464e5e36856e91f312240d48b4642b3bf4
Nicolas Uchuvo <nicolasuchuvo123@gmail.com>
2026-03-29 18:36:48 -0500

    Added Logistic Regression module

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def application():
     )
 @app.route("/LogisticRegression")
 def LogisticRegression():
-    return render_template('HomeLogisticRegression')
+    return render_template('HomeLogisticRegression.html')
